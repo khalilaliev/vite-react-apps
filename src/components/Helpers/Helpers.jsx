d9df8b2d15805a8030041813e6a4faea51019498
khalilaliev <khalilaliev0@gmail.com>
2024-06-27 14:09:50 +0200

    added some commits

--- src/components/Helpers/Helpers.jsx
+++ src/components/Helpers/Helpers.jsx
@@ -1,3 +1,1 @@
 export const API = "https://jsonplaceholder.typicode.com/users";
-export const MAPBOX_TOKEN =
-  "pk.eyJ1Ijoia2hhbGlsYWxpZXYiLCJhIjoiY2x3ODBhdWR5MXp1MDJscGR0eGgxMXh2aSJ9.ppOwu4hFatGJKjiMFvoDZA";
